9f88406104a04c1223b87a911d06ca6c7dcf269d
JohannGourmelon <131705982+JohannGourmelon@users.noreply.github.com>
2024-10-08 23:45:06 +0200

    Update domBuilder.js

--- js/domBuilder.js
+++ js/domBuilder.js
@@ -1,8 +1,9 @@
-// Fonction pour créer une section HTML avec un titre et du contenu
-function createSection(title, content) {
+// Fonction pour créer une section HTML avec un titre, du contenu, et une classe spécifique
+function createSection(title, content, className) {
   const section = document.createElement("section");
   const h4 = document.createElement("h4");
   h4.textContent = title;
+  section.classList.add(className); // Ajout de la classe spécifique
   section.appendChild(h4);
   section.appendChild(content);
   document.querySelector(".cv-sections").appendChild(section);
@@ -13,13 +14,11 @@ function createPersonalInfoSection(cvData) {
   const div = document.createElement("div");
   div.classList.add("text-center");
   div.innerHTML = `
-          <h1>${cvData.nom || "Nom non disponible"}</h1>
-          <p>${cvData.adresse || "Adresse non disponible"} | ${
-    cvData.email || "Email non disponible"
-  } | ${cvData.telephone || "Téléphone non disponible"}</p>
-          <h3>${cvData.profession || "Profession non disponible"}</h3>
-      `;
-  document.querySelector(".cv-sections").appendChild(div);
+        <h1>${cvData.nom || "Nom non disponible"}</h1>
+        <p>${cvData.adresse || "Adresse non disponible"} | ${cvData.email || "Email non disponible"} | ${cvData.telephone || "Téléphone non disponible"}</p>
+        <h3>${cvData.profession || "Profession non disponible"}</h3>
+    `;
+  createSection("Informations personnelles", div, "personal-info");
 }
 
 // Fonctions pour créer les différentes sections du CV
@@ -28,13 +27,8 @@ function createTechnicalSkillsSection(competenceData) {
   Object.keys(competenceData).forEach((key) => {
     const competence = competenceData[key];
     const li = document.createElement("li");
-    li.innerHTML = `<strong>${
-      competence.intitule || "Intitulé non disponible"
-    } :</strong>`;
-    if (
-      Array.isArray(competence.description) &&
-      typeof competence.description[0] === "object"
-    ) {
+    li.innerHTML = `<strong>${competence.intitule || "Intitulé non disponible"} :</strong>`;
+    if (Array.isArray(competence.description) && typeof competence.description[0] === "object") {
       const innerUl = document.createElement("ul");
       competence.description.forEach((item) => {
         const subLi = document.createElement("li");
@@ -44,13 +38,11 @@ function createTechnicalSkillsSection(competenceData) {
       });
       li.appendChild(innerUl);
     } else {
-      li.innerHTML += ` ${
-        competence.description || "Description non disponible"
-      }`;
+      li.innerHTML += ` ${competence.description || "Description non disponible"}`;
     }
     ul.appendChild(li);
   });
-  createSection("Compétences Techniques", ul);
+  createSection("Compétences Techniques", ul, "technical-skills");
 }
 
 function createFunctionalSkillsSection(competenceFonctionnelleData) {
@@ -58,12 +50,10 @@ function createFunctionalSkillsSection(competenceFonctionnelleData) {
   Object.keys(competenceFonctionnelleData).forEach((key) => {
     const competence = competenceFonctionnelleData[key];
     const li = document.createElement("li");
-    li.innerHTML = `<strong>${
-      competence.intitule || "Intitulé non disponible"
-    } :</strong> ${competence.description || "Description non disponible"}`;
+    li.innerHTML = `<strong>${competence.intitule || "Intitulé non disponible"} :</strong> ${competence.description || "Description non disponible"}`;
     ul.appendChild(li);
   });
-  createSection("Compétences Fonctionnelles", ul);
+  createSection("Compétences Fonctionnelles", ul, "functional-skills");
 }
 
 function createFormationSection(formationData) {
@@ -71,12 +61,10 @@ function createFormationSection(formationData) {
   Object.keys(formationData).forEach((key) => {
     const formation = formationData[key];
     const li = document.createElement("li");
-    li.innerHTML = `${formation.dates_debut || ""} : <strong>${
-      formation.diplome
-    }</strong> - ${formation.etablissement}`;
+    li.innerHTML = `${formation.dates_debut || ""} : <strong>${formation.diplome}</strong> - ${formation.etablissement}`;
     ul.appendChild(li);
   });
-  createSection("Diplômes et Formations", ul);
+  createSection("Diplômes et Formations", ul, "formation");
 }
 
 function createExperiencesSection(experienceData) {
@@ -84,9 +72,7 @@ function createExperiencesSection(experienceData) {
   Object.keys(experienceData).forEach((key) => {
     const experience = experienceData[key];
     const li = document.createElement("li");
-    li.innerHTML = `<strong>${experience.dates_debut || ""} – ${
-      experience.dates_fin || ""
-    } : ${experience.poste} – ${experience.entreprise}</strong>`;
+    li.innerHTML = `<strong>${experience.dates_debut || ""} – ${experience.dates_fin || ""} : ${experience.poste} – ${experience.entreprise}</strong>`;
     const innerUl = document.createElement("ul");
     experience.description.forEach((desc) => {
       const subLi = document.createElement("li");
@@ -96,7 +82,7 @@ function createExperiencesSection(experienceData) {
     li.appendChild(innerUl);
     ul.appendChild(li);
   });
-  createSection("Expériences", ul);
+  createSection("Expériences", ul, "experiences");
 }
 
 function createInterestsSection(centresInteretData) {
@@ -107,5 +93,5 @@ function createInterestsSection(centresInteretData) {
     li.textContent = `${centre.titre} : ${centre.description}`;
     ul.appendChild(li);
   });
-  createSection("Centres d'intérêt", ul);
+  createSection("Centres d'intérêt", ul, "interests");
 }
