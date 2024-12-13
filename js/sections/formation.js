2a3fcfd1492386138c801edf966b31bce3e47425
JohannGourmelon <johann.gourmelon@gmail.com>
2024-12-13 15:19:12 +0100

    modif date de fin

--- js/sections/formation.js
+++ js/sections/formation.js
@@ -1,10 +1,12 @@
 function createFormationSection(formationData) {
-    const ul = document.createElement("ul");
-    Object.keys(formationData).forEach((key) => {
-        const formation = formationData[key];
-        const li = document.createElement("li");
-        li.innerHTML = `${formation.dates_debut || ""} : <strong>${formation.diplome}</strong> - ${formation.etablissement}`;
-        ul.appendChild(li);
-    });
-    createSection("Diplômes et Formations", ul, "formation");
+  const ul = document.createElement("ul");
+  Object.keys(formationData).forEach((key) => {
+    const formation = formationData[key];
+    const li = document.createElement("li");
+    li.innerHTML = `${formation.dates_fin || ""} : <strong>${
+      formation.diplome
+    }</strong> - ${formation.etablissement}`;
+    ul.appendChild(li);
+  });
+  createSection("Diplômes et Formations", ul, "formation");
 }
