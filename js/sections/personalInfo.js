f40715e337ba608233c9bf1f173ec7779d143792
JohannGourmelon <131705982+JohannGourmelon@users.noreply.github.com>
2024-12-30 17:40:12 +0100

    Update personalInfo.js

--- js/sections/personalInfo.js
+++ js/sections/personalInfo.js
@@ -3,7 +3,7 @@ function createPersonalInfoSection(cvData) {
   div.classList.add("text-center");
   div.innerHTML = `
     <h1>${cvData.nom || "Nom non disponible"}</h1>
-    <p>${cvData.adresse || "Adresse non disponible"} | ${cvData.email || "Email non disponible"} | ${cvData.telephone || "Téléphone non disponible"}</p>
+    <p>${cvData.adresse || "Adresse non disponible"} | ${cvData.email || "Email non disponible"} | ${cvData.telephone || "Téléphone non disponible"} | ${cvData.age|| "Age non disponible"}</p>
     <h3>${cvData.profession || "Profession non disponible"}</h3>`;
   
   createSection("Informations personnelles", div, "personal-info");
